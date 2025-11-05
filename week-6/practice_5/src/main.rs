08a7609ffe450e7f4c35603857a9f35b72829382
00bal <obaloluwa.omoyajowo@pau.edu.ng>
2025-11-05 13:49:23 +0100

    (Empty string len boolian)

--- week-6/practice_5/src/main.rs
+++ week-6/practice_5/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let fullname = "Omoyajowo Obaloluwa Similoluwa";
+    println!();
+    println!("Name: {}", fullname);
+    println!();
+    println!("Before trim");
+    println!("Length is {}", fullname.len());
+    println!();
+    println!("After trim");
+    println!("lenth is {}", fullname.trim().len());
 }
