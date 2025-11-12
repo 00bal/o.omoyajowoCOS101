23125dfbcad65f402fe26d9384d095792db8a09e
00bal <obaloluwa.omoyajowo@pau.edu.ng>
2025-11-12 13:33:54 +0100

    (Invoking a function)

---
+++ week-7/practice1/src/main.rs
@@ -0,0 +1,11 @@
+fn my_grade(){
+
+    //funtion body
+    println!("Greetings from function my_grade!");
+}
+
+fn main(){
+
+    //calling function
+    my_grade();
+}
---
+++ week-7/practice1/target/debug/practice1.d
@@ -0,0 +1,1 @@
+C:\Users\Ectos\OneDrive\Documents\o.omoyajowoCOS101\week-7\practice1\target\debug\practice1.exe: C:\Users\Ectos\OneDrive\Documents\o.omoyajowoCOS101\week-7\practice1\src\main.rs
