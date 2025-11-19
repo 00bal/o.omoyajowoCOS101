319d7cdd0761d624c433506008ecd601676994bd
00bal <obaloluwa.omoyajowo@pau.edu.ng>
2025-11-19 13:25:57 +0100

    (Use of get in vector)

---
+++ week-8/practice3/target/debug/deps/practice3.d
@@ -0,0 +1,5 @@
+C:\Users\Ectos\OneDrive\Documents\o.omoyajowoCOS101\week-8\practice3\target\debug\deps\practice3.d: src\main.rs
+
+C:\Users\Ectos\OneDrive\Documents\o.omoyajowoCOS101\week-8\practice3\target\debug\deps\practice3.exe: src\main.rs
+
+src\main.rs:
