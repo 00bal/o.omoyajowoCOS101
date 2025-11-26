c42c4f7d9fbc1896e27abac583629ad96be43f3a
00bal <obaloluwa.omoyajowo@pau.edu.ng>
2025-11-26 13:24:19 +0100

    week9

--- week-9/practice3/src/main.rs
+++
@@ -1,6 +0,0 @@
-use std::fs;
-
-fn main() {
-    fs::remove_file("data.txt").expect("Could not remove file");
-    println!("File is removed");
-}
--- week-9/practice3/target/debug/deps/practice3.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Ectos\OneDrive\Documents\o.omoyajowoCOS101\week-9\practice3\target\debug\deps\practice3.d: src\main.rs
-
-C:\Users\Ectos\OneDrive\Documents\o.omoyajowoCOS101\week-9\practice3\target\debug\deps\practice3.exe: src\main.rs
-
-src\main.rs:
