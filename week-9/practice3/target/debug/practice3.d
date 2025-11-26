04f97c6bb5cb6dd0109aa55fdcaa058dc3f23d1d
00bal <obaloluwa.omoyajowo@pau.edu.ng>
2025-11-26 13:24:19 +0100

    Delete week-9 directory

--- week-9/practice3/target/debug/practice3.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Ectos\OneDrive\Documents\o.omoyajowoCOS101\week-9\practice3\target\debug\practice3.exe: C:\Users\Ectos\OneDrive\Documents\o.omoyajowoCOS101\week-9\practice3\src\main.rs
--- week-9/practice4/target/debug/deps/practice4.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Ectos\OneDrive\Documents\o.omoyajowoCOS101\week-9\practice4\target\debug\deps\practice4.d: src\main.rs
-
-C:\Users\Ectos\OneDrive\Documents\o.omoyajowoCOS101\week-9\practice4\target\debug\deps\practice4.exe: src\main.rs
-
-src\main.rs:
